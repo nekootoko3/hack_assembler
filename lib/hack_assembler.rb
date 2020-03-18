38272ea4ef0fd389401f9975c310308caa58ef3a
nekootoko3 <miyako.kerria@gmail.com>
2020-03-18 13:18:55 +0900

    bundle gem hack_assembler

---
+++ lib/hack_assembler.rb
@@ -0,0 +1,6 @@
+require "hack_assembler/version"
+
+module HackAssembler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hack_assembler/version.rb
@@ -0,0 +1,3 @@
+module HackAssembler
+  VERSION = "0.1.0"
+end
