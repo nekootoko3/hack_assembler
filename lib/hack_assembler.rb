2fa00938217e7cc9798033ad5b28dc36b6c4cfab
nekootoko3 <miyako.kerria@gmail.com>
2020-03-19 13:27:12 +0900

    Implement cli interface

--- lib/hack_assembler.rb
+++ lib/hack_assembler.rb
@@ -1,4 +1,5 @@
 require "hack_assembler/version"
+require "hack_assembler/cli"
 
 module HackAssembler
   class Error < StandardError; end
---
+++ lib/hack_assembler/cli.rb
@@ -0,0 +1,6 @@
+require "hack_assembler"
+
+class HackAssembler::Cli
+  def self.start(input_file, output_file)
+  end
+end
